da865ebab2e08a47ef850a061943034dda64d23a
KolchevAI <125642640+KolchevAI@users.noreply.github.com>
2023-03-26 20:45:36 +0300

    Create osb.cpp

---
+++ 6 practice/3 task/osb.cpp
@@ -0,0 +1,17 @@
+#include "osb.h"
+
+istream& operator>>(istream& in, Ocean& ocean) {
+    cout << "Enter name: " << endl;
+    in >> ocean.name;
+    cout << "Enter size: " << endl;
+    in >> ocean.size;
+    cout << "Enter depth: " << endl;
+    in >> ocean.depth;
+    return in;
+}
+ostream& operator<<(ostream& out, const Ocean& ocean) {
+    out << "Name: " << ocean.name << endl;
+    out << "Size: " << ocean.size << endl;
+    out << "Depth: " << ocean.depth << endl;
+    return out;
+}
